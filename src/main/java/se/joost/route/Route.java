31156ca97fa196b92acc55a29bef0df5df4eeb7d
remijnj <remijnj@gmail.com>
2018-06-30 16:32:48 +0200

    First working version with mysql database

--- src/main/java/se/joost/route/Route.java
+++ src/main/java/se/joost/route/Route.java
@@ -1,19 +1,31 @@
 package se.joost.route;
 
-public class Route {
-    private final long id;
-    private final String name;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-    public Route(long id, String name) {
-        this.id = id;
-        this.name = name;
+@Entity // This tells Hibernate to make a table out of this class
+public class Route {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
     }
 
-    public long getId() {
-        return id;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/se/joost/route/RouteController.java
+++ src/main/java/se/joost/route/RouteController.java
@@ -1,22 +1,32 @@
 package se.joost.route;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiOperation;
 
+//import se.joost.route.RouteRepository;
+//import se.joost.route.Route;
+
 @RestController
 public class RouteController {
     private final AtomicLong counter = new AtomicLong();
+    
+    @Autowired 
+    private RouteRepository routeRepository;
 
     @RequestMapping(value = "/new", method = RequestMethod.PUT)
     @ApiOperation(
         value = "Create a new route",
         notes = "This creates a route to which stops can be added later.")
     public Route newroute(@RequestParam(value="name") String name) {
-        return new Route(counter.incrementAndGet(), name);
+        Route r = new Route();
+        r.setName(name);
+        routeRepository.save(r);
+        return r;
     }
 
     @RequestMapping(value = "/load", method = RequestMethod.GET)
